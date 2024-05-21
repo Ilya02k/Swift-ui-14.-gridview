ad3d393030b474f6e74aa43af4e6a6b6f6342c00
Ilya Kazlou <ilya02k@gmail.com>
2024-05-21 13:00:39 +0300

    swift ui 14

---
+++ swift ui 14/swift ui 14/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift ui 14
+//
+//  Created by Ann Dosova on 20.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift ui 14/swift ui 14/swift_ui_14App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_14App.swift
+//  swift ui 14
+//
+//  Created by Ann Dosova on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
